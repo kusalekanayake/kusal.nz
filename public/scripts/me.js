a45da6bb3c6d48e5344a1376bab7a14f979ded23
Kusal Ekanayake <kusalekanayake@yahoo.co.nz>
2022-07-07 15:24:26 +1200

    Overhauled new website from webflow

--- public/scripts/me.js
+++
@@ -1,6 +0,0 @@
-const tl = gsap.timeline({defaults: {ease: "power1.out"}});
-
-
-// tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1});
-// tl.fromTo(".title", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
-tl.fromTo(".column", {opacity: 0}, {opacity: 1, duration: 1});
